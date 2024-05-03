00b1dd416e7bcf39349eb65671faf7be7bb1fa9a
JanikNex <18660622+JanikNex@users.noreply.github.com>
2024-05-03 14:33:17 +0200

    readded Eclipse OCL, partially reverting 75da05430f12a73e4cbfef6dd28cae966097ca4e

--- trainbenchmark-scripts/src-template/BenchmarkScript.groovy
+++ trainbenchmark-scripts/src-template/BenchmarkScript.groovy
@@ -3,6 +3,7 @@ import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfigBuilder
 import hu.bme.mit.trainbenchmark.benchmark.config.ModelSetConfig
 import hu.bme.mit.trainbenchmark.benchmark.config.TransformationChangeSetStrategy
 import hu.bme.mit.trainbenchmark.benchmark.emfapi.config.EmfApiBenchmarkConfigBuilder
+import hu.bme.mit.trainbenchmark.benchmark.eclipseocl.config.EclipseOclBenchmarkConfigBuilder
 import hu.bme.mit.trainbenchmark.benchmark.jena.config.JenaBenchmarkConfigBuilder
 import hu.bme.mit.trainbenchmark.benchmark.mysql.config.MySqlBenchmarkConfigBuilder
 import hu.bme.mit.trainbenchmark.benchmark.neo4j.config.Neo4jBenchmarkConfigBuilder
@@ -54,6 +55,7 @@ println()
 
 def tools = [
         new EmfApiBenchmarkConfigBuilder(),
+		new EclipseOclBenchmarkConfigBuilder(),
         new JenaBenchmarkConfigBuilder().setInferencing(false).setFormat(RdfFormat.TURTLE),
         new JenaBenchmarkConfigBuilder().setInferencing(false).setFormat(RdfFormat.NTRIPLES),
         new JenaBenchmarkConfigBuilder().setInferencing(true).setFormat(RdfFormat.TURTLE),
